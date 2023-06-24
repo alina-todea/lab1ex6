16bcbc101a875cdf9caf54bda82c1d9141d6d9ed
alina-todea <137332096+alina-todea@users.noreply.github.com>
2023-06-24 09:33:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace lab1ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine descrescatoare.
+                • Exemplu: citim 3,9,0 Afisam : 9 3 0
+            */
+
+            Console.WriteLine("introduceti primul numar");
+            int numar1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduceti al doilea numar");
+            int numar2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("introduceti al treilea numar");
+            int numar3 = int.Parse(Console.ReadLine());
+
+            if (numar1 >= numar2 && numar2 >= numar3)
+                Console.WriteLine(numar1 + " " + numar2 + " " + numar3);
+            else if
+                (numar1 >= numar2 && numar2 < numar3 && numar1>=numar3)
+                Console.WriteLine(numar1 + " " + numar3 + " " + numar2);
+            else if (numar1 >= numar2 && numar2 < numar3 && numar1 < numar3)
+                Console.WriteLine(numar3 + " " + numar1 + " " + numar2);
+            else if
+                (numar1 < numar2 && numar2 < numar3)
+                Console.WriteLine(numar3 + " " + numar2 + " " + numar1);
+            else if
+                (numar1 < numar2 && numar2 >= numar3 && numar1 >= numar3)
+                Console.WriteLine(numar2 + " " + numar1 + " " + numar3);
+            else if ( numar1 < numar3)
+                Console.WriteLine(numar2 + " " + numar3 + " " + numar1);
+        }
+    }
+}
